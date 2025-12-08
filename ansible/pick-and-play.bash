ad2a6cb151436c7facc37ed1d421cf058207c786
MBahyeldin <muhammad.baheuddeen@gmail.com>
2025-12-08 14:51:41 +0200

    update lxd default profile to allow static ips

--- ansible/pick-and-play.bash
+++ ansible/pick-and-play.bash
@@ -2,7 +2,7 @@
 
 # Define the commands
 commands=(
-  "ansible-playbook   hosting-machine.playbook.yml      -i inventory/hosting-machine.inventory.yml                             --vault-password-file ./internal-scripts/vault-pass.bash"
+  "ansible-playbook   hosting-machine.playbook.yml      -i inventory/hosting-machine.inventory.yml                             --vault-password-file ./internal-scripts/vault-pass.bash --ask-become-pass"
   "ansible-playbook   redis.playbook.yml                -i inventory/redis.inventory.yml           -e @vars/redis_vault.yml    --vault-password-file ./internal-scripts/vault-pass.bash"
   "ansible-playbook   app.playbook.yml                  -i inventory/app.inventory.yml             -e @vars/app_vault.yml      --vault-password-file ./internal-scripts/vault-pass.bash"
   "ansible-playbook   postgres.playbook.yml             -i inventory/postgres.inventory.yml        -e @vars/postgres_vault.yml --vault-password-file ./internal-scripts/vault-pass.bash"
